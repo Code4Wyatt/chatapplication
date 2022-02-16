aee8dbcfc2fc33df41a19489870ade70d61d3262
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-02-16 15:19:32 +0000

    Update

---
+++ server/src/App.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import { createServer } from "http"
+import { Server } from "socket.io"
+import cors from "cors"
+import config from "config"
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
