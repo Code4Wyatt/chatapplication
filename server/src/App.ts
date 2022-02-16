39c676b7f793e57fa75b6011ed4c3df02a6ec598
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-02-16 15:56:34 +0000

    Update

--- server/src/App.ts
+++ server/src/App.ts
@@ -3,6 +3,27 @@ import { createServer } from "http"
 import { Server } from "socket.io"
 import cors from "cors"
 import config from "config"
+import logger from "./utils/logger"
+import { version } from "../package.json"
 
-const port = config.get<number>("port");
-const host = config.get<string>("host");
+const port = config.get<number>("port")
+const host = config.get<string>("host")
+const corsOrigin = config.get<string>("corsOrigin")
+
+const app = express()
+
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: corsOrigin,
+        credentials: true,
+    },
+});
+
+app.get("/", (_, res) => res.send(`Server is up and running version: ${version}`))
+
+httpServer.listen(port, host, () => {
+    logger.info("Server is listening")
+    logger.info(`http://${host}:${port}`)
+})
